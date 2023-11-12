fcc1326b4fdfba11bfe536ea0322241d01bff033
khongchai <world1955@hotmail.com>
2023-11-13 01:29:16 +0700

    uncomment other benches

--- bench/benchmark.js
+++ bench/benchmark.js
@@ -38,41 +38,41 @@ const options = {
     },
 }
 
-// new Benchmark.Suite(`Insert (x${N})`, options)
-//   .add('Bintrees', () => {
-//     let rb = new RBTree((a, b) => a - b);
-//     for (let i = 0; i < N; i++) rb.insert(rvalues[i]);
-//   })
-//   .add('Functional red black tree', () => {
-//     let frb = new FRB();
-//     for (let i = 0; i < N; i++) frb = frb.insert(rvalues[i]);
-//   })
-//   .add('Google Closure library AVL', () => {
-//     let gcavl = new goog.structs.AvlTree((a, b) => a - b);
-//     for (let i = 0; i < N; i++) gcavl.add(rvalues[i]);
-//   })
-//   .add('AVL (current)', () => {
-//     const tree = new Tree();
-//     for (let i = 0; i < N; i++) tree.insert(rvalues[i]);
-//   })
-//   .run();
+new Benchmark.Suite(`Insert (x${N})`, options)
+    .add('Bintrees', () => {
+        let rb = new RBTree((a, b) => a - b)
+        for (let i = 0; i < N; i++) rb.insert(rvalues[i])
+    })
+    .add('Functional red black tree', () => {
+        let frb = new FRB()
+        for (let i = 0; i < N; i++) frb = frb.insert(rvalues[i])
+    })
+    .add('Google Closure library AVL', () => {
+        let gcavl = new goog.structs.AvlTree((a, b) => a - b)
+        for (let i = 0; i < N; i++) gcavl.add(rvalues[i])
+    })
+    .add('AVL (current)', () => {
+        const tree = new Tree()
+        for (let i = 0; i < N; i++) tree.insert(rvalues[i])
+    })
+    .run()
 
-// new Benchmark.Suite(`Random read (x${N})`, options)
-//     .add('Bintrees', () => {
-//         for (let i = N - 1; i; i--) prefilledRB.find(rvalues[i])
-//     })
-//     .add('Functional red black tree', () => {
-//         for (let i = N - 1; i; i--) prefilledFRB.get(rvalues[i])
-//     })
-//     .add('Google Closure library AVL', () => {
-//         for (let i = 0; i < N; i++)
-//             prefilledGCAVL.inOrderTraverse((v) => v === rvalues[i])
-//     })
-//     .add('AVL (current)', () => {
-//         for (let i = N - 1; i; i--) prefilledAVL.find(rvalues[i])
-//     })
+new Benchmark.Suite(`Random read (x${N})`, options)
+    .add('Bintrees', () => {
+        for (let i = N - 1; i; i--) prefilledRB.find(rvalues[i])
+    })
+    .add('Functional red black tree', () => {
+        for (let i = N - 1; i; i--) prefilledFRB.get(rvalues[i])
+    })
+    .add('Google Closure library AVL', () => {
+        for (let i = 0; i < N; i++)
+            prefilledGCAVL.inOrderTraverse((v) => v === rvalues[i])
+    })
+    .add('AVL (current)', () => {
+        for (let i = N - 1; i; i--) prefilledAVL.find(rvalues[i])
+    })
 
-//     .run()
+    .run()
 
 new Benchmark.Suite(`Random interval read (x${N})`, options)
     .add('AVL (current)', () => {
@@ -80,33 +80,34 @@ new Benchmark.Suite(`Random interval read (x${N})`, options)
     })
     .run()
 
-// new Benchmark.Suite(`Remove (x${N})`, options)
-//   .add('Bintrees', () => {
-//     for (let i = 0; i < N; i++) prefilledRB.remove(rvalues[i]);
-//   })
-//   .add('Functional red black tree', () => {
-//     for (let i = 0; i < N; i++) prefilledFRB = prefilledFRB.remove(rvalues[i]);
-//   })
-//   .add('Google Closure library AVL', () => {
-//     for (let i = 0; i < N; i++) prefilledGCAVL.remove(rvalues[i]);
-//   })
-//   .add('AVL (current)', () => {
-//     for (let i = N - 1; i; i--) prefilledAVL.remove(values[i]);
-//   })
-//   .run();
+new Benchmark.Suite(`Remove (x${N})`, options)
+    .add('Bintrees', () => {
+        for (let i = 0; i < N; i++) prefilledRB.remove(rvalues[i])
+    })
+    .add('Functional red black tree', () => {
+        for (let i = 0; i < N; i++)
+            prefilledFRB = prefilledFRB.remove(rvalues[i])
+    })
+    .add('Google Closure library AVL', () => {
+        for (let i = 0; i < N; i++) prefilledGCAVL.remove(rvalues[i])
+    })
+    .add('AVL (current)', () => {
+        for (let i = N - 1; i; i--) prefilledAVL.remove(values[i])
+    })
+    .run()
 
-// const M = 10000;
-// const arr = new Array(M).fill(0).map((i) => M * Math.random());
-// arr.sort((a, b) => a - b);
-// new Benchmark.Suite(`Bulk-load (x${M})`, options)
-//   .add('1 by 1', () => {
-//     const t = new Tree();
-//     for (let i = 0; i < M; i++) t.insert(arr[i]);
-//   })
-//   .add('bulk load', () => {
-//     const t = new Tree();
-//     const data = arr.slice();
+const M = 10000
+const arr = new Array(M).fill(0).map((i) => M * Math.random())
+arr.sort((a, b) => a - b)
+new Benchmark.Suite(`Bulk-load (x${M})`, options)
+    .add('1 by 1', () => {
+        const t = new Tree()
+        for (let i = 0; i < M; i++) t.insert(arr[i])
+    })
+    .add('bulk load', () => {
+        const t = new Tree()
+        const data = arr.slice()
 
-//     t.load(data, []);
-//   })
-//   .run();
+        t.load(data, [])
+    })
+    .run()
